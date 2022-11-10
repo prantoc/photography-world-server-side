a9b16762274526775d295c0ffa78bf67b816ddc1
Pranto Chakraborty <chakrabortypranto39@gmail.com>
2022-11-10 11:44:54 +0600

    service post api

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express')
 const cors = require('cors')
 const jwt = require('jsonwebtoken');
 require('dotenv').config()
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+
 const port = process.env.PORT || 5000
 
 const app = express()
@@ -11,6 +13,33 @@ app.use(express.json())
 app.get('/', (req, res) => {
     res.send('Photography-World server side is running......')
 })
+
+
+//!database connection details
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.7incky7.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+
+async function run() {
+    try {
+        const serviceCollection = client.db('phWorld').collection('services')
+
+        //service api
+        app.post('/add-service', async (req, res) => {
+            const service = req.body
+            const result = await serviceCollection.insertOne(service);
+            res.send(result)
+        })
+
+    } catch (error) {
+
+    }
+}
+run().catch(console.dir);
+
+
 
 
 app.listen(port, () => {
